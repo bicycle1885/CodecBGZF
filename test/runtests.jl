b6ab040aa93e6e0a703c51274e59b4094108121a
Kenta Sato <bicycle1885@gmail.com>
2017-11-01 14:40:37 +0900

    fixup! fixup! fixup! fixup! add BGZFCompressor and BGZFDecompressor

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,8 @@ using Base.Test
 
 @testset "BGZF Codec" begin
     # Test the EOF trailer (28 bytes).
-    buffer = IOBuffer()
+    data = UInt8[]
+    buffer = IOBuffer(data, true, true)
     stream = BGZFCompressorStream(buffer)
     write(stream, "foobar")
     data = buffer.data
@@ -20,6 +21,7 @@ using Base.Test
         0x00, 0xff, 0x06, 0x00, 0x42, 0x43, 0x02, 0x00,
         0x1b, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00]
+    @test read(BGZFDecompressorStream(IOBuffer(data))) == b"foobar"
 
     test_roundtrip_read(BGZFCompressorStream, BGZFDecompressorStream)
     test_roundtrip_write(BGZFCompressorStream, BGZFDecompressorStream)
